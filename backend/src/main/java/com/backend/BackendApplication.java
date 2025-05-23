9b2d2ecf9be16a9eca4aab138a4b7ac29190e519
gregorioRQ <rafaelgq2000@gmail.com>
2025-05-23 10:56:46 -0300

    configuracion para que la app pueda leer las variables de entorno

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,12 +1,26 @@
 package com.backend;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new File(".env").getAbsolutePath());
+		Dotenv dotenv = Dotenv.configure()
+				.directory("../") // Ruta relativa al archivo .env
+				.load();
+
+		// Establece las variables de entorno para Spring Boot
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
